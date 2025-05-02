3915c242e47e4d13e5ada7274849a012cd353583
Sambiv <sambiv.dahal@ucdenver.edu>
2025-05-02 01:19:45 -0600

    Update Reviews.js

--- Reviews.js
+++ Reviews.js
@@ -3,15 +3,13 @@ var Schema = mongoose.Schema;
 
 mongoose.connect(process.env.DB);
 
-// Movie schema
+// Review schema
 var ReviewSchema = new Schema({
     movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
     username: { type: String, required: true },
     review: { type: String, required: true },
     rating: { type: Number, min: 0, max: 5, required: true }
 });
-
-ReviewSchema.index({ movieId: 1, username: 1 }, { unique: true });
 
 // return the model
 module.exports = mongoose.model('Review', ReviewSchema);
